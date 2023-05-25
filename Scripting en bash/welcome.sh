0d10e4935c913a4127494f0f65778d8b356fa887
Isaias <111402296+Isaias919@users.noreply.github.com>
2023-05-25 02:05:16 -0600

    Update welcome.sh

--- Scripting en bash/welcome.sh
+++ Scripting en bash/welcome.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Script welcome.sh
 # 27/09/2022 - Isaias Emiliano Colunga Santos
-
+echo "Hola ${LOGNAME}"
+echo "Hoy es $(date)"
+echo "Usuarios actuales conectados, y sus procesos:"
